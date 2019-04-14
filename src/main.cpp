271ba18a231a5560538f07cd8ab62cd66f6a6b56
jlahman <jlahman@users.noreply.github.com>
2019-04-13 23:08:34 -0400

    rwar

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include"Track.h"
 #include"util.h"
 #include"AnimationPlayer.h"
-#include"AudioPlayer.h"
 
 #define NUM_SECONDS   (16)
 std::vector<Track*> trackList;
@@ -15,16 +14,13 @@ std::vector<Track*> trackList;
 int main(int argc,  char * argv[])
 {
 
-    PaError err;
+    //PaError err;
     //AudioPlayer* ap;
-    AnimationPlayer * anime;
-	//ap = new AudioPlayer();
-	anime = new AnimationPlayer("../data/CIPIC_hrtf_database/standard_hrir_database/subject_058/hrir_final.mat");
+    AnimationPlayer * anime = new AnimationPlayer("../data/CIPIC_hrtf_database/standard_hrir_database/subject_058/hrir_final.mat");
 
     trackList.push_back(new Track("../data/test.wav"));
 
-
-	anime->addSource("Narrator2", trackList[0]);
+	//anime->addSource("Narrator2", trackList[0]);
 
 
 	const int framesPerAnimationStep = 4096;
@@ -40,7 +36,7 @@ int main(int argc,  char * argv[])
 	overflow[0] = new double[200-1];
 	overflow[1]= new double[200-1];
 	double *audioOutInterlaced = new double[audioOutSize];
-	
+
 	anime->getBuffer(audioOut, overflow, frameCount, (const int)frameStop);
 	for(int i = 0; i < frameStop; i++){
 		//printf("%E\n", audioOut[0][i]);
@@ -53,16 +49,16 @@ int main(int argc,  char * argv[])
 	printf("%d\n", frameStop*2-1);
 
 
-	delete[] audioOut[0];
-	delete[] audioOut[1];
-	delete[] audioOut;
+	//delete[] audioOut[0];
+	//delete[] audioOut[1];
+	//delete[] audioOut;
 	printf("PortAudio Test\n");
 
-	delete[] overflow[0];	printf("PortAudio Test\n");
+	//delete[] overflow[0];	printf("PortAudio Test\n");
 
-	delete[] overflow[1];	printf("PortAudio Test\n");
+	//delete[] overflow[1];	printf("PortAudio Test\n");
 
-	delete[] overflow;	printf("PortAudio Test\n");
+	//delete[] overflow;	printf("PortAudio Test\n");
 
 
 	printf("PortAudio Test\n");
